acf9a47f7e87b81e8c0be570a3675bf736fbd2b3
kenswot <68135936+kenswot@users.noreply.github.com>
2020-10-19 12:05:53 +0100

    Updates

---
+++ precache-manifest.e0054830551cad465289a7c5634c9895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ca79a12c949a7577f5ee4d02c7327a",
+    "url": "/voice-alan/index.html"
+  },
+  {
+    "revision": "d779d0884c1e3c6105b6",
+    "url": "/voice-alan/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "735e19b16d009a4b2632",
+    "url": "/voice-alan/static/js/2.2bad6f1e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/voice-alan/static/js/2.2bad6f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d779d0884c1e3c6105b6",
+    "url": "/voice-alan/static/js/main.cdf9d289.chunk.js"
+  },
+  {
+    "revision": "bc9c99525577c68374b5",
+    "url": "/voice-alan/static/js/runtime-main.11dad3bb.js"
+  }
+]);
