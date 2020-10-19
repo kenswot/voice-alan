bc67df0141eff7643919cd33e0923abc9bcd2b0d
kenswot <68135936+kenswot@users.noreply.github.com>
2020-10-19 11:32:02 +0100

    'alan'

---
+++ build/precache-manifest.5524fb20a3fdf6e0158e1b0c305484b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5af4da11e2721b34c9afcc99ae6209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bf473c055c2a379ec1e",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "735e19b16d009a4b2632",
+    "url": "/static/js/2.2bad6f1e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.2bad6f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf473c055c2a379ec1e",
+    "url": "/static/js/main.37c78742.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  }
+]);
